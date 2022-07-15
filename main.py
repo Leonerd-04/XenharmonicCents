bf26dffed7f14ee3ca5db64fb6ad94126aa0fb82
Ieonard04 <leonerd04.2718@gmail.com>
2022-07-14 20:41:33 -0700

    Changed the script to use integer fractions, and also to print the ratio with fractions

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from math import log2
+from math import log2, gcd
+
+
+# Multiplies, then reduces, two fractions
+def multiply(frac0: tuple[int, int], frac1: tuple[int, int]) -> tuple[int, int]:
+    result = frac0[0] * frac1[0], frac0[1] * frac1[1]
+    factor = gcd(result[0], result[1])
+    return result[0] // factor, result[1] // factor
 
 
 # One step = one half-step (eg. C to Db)
@@ -25,72 +32,49 @@ def note_to_12TET_steps(name: str) -> int:
     return steps
 
 
-# Determines how many factors of 3 a natural note is away from C
-# Tuple is (factors of 2, factors of 3)
+# Assigns a fraction to each note name as a ratio back to C
 def note_name_to_factor(name: str) -> tuple[int, int]:
     switch = {
-        'F': (2, -1),
-        'C': (0, 0),
-        'G': (-1, 1),
-        'D': (-3, 2),
-        'A': (-4, 3),
-        'E': (-6, 4),
-        'B': (-7, 5),
+        'F': (4, 3),
+        'C': (1, 1),
+        'G': (3, 2),
+        'D': (9, 8),
+        'A': (27, 16),
+        'E': (81, 64),
+        'B': (243, 128),
     }
 
     return switch.get(name[0])
 
 
-def parse_factors(note_raw: str) -> dict[int, int]:
-    factors = {
-        2: 0,
-        3: 0,
-        5: 0,
-        7: 0,
-        11: 0,
-        13: 0
-    }
+def parse_input(note_raw: str) -> tuple[int, int]:
+    # Starts on the Pythagorean note name
+    note_fraction = note_name_to_factor(note_raw[0])
 
-    # Tuple structure is as unpacked below: (factor, amount, correction2, correction3)
-    # factor should always be a prime number and is the accidental's primary factor
-    # amount is the exponent of the primary factor, will generally be 1 or -1
-    # The two corrections are the factors of 2 and 3 respectively.
-    # Adjacent accidentals are inverses.
+    # Fractional ratios for the different available accidentals
+    # Adjacent accidentals are inverses of each other
     accidentals = {
-        '#': (3, 7, -11, 0),    # Sharp and flat are pythagorean
-        'b': (3, -7, 11, 0),    # '#' = 2187 / 2048
+        '#': (2187, 2048),  # Sharp and flat are pythagorean
+        'b': (2048, 2187),  # '#' = 3^7 / 2^11
 
-        '-': (5, 1, 4, -4),     # Syntonic comma
-        '+': (5, -1, -4, 4),    # '-' = 80 / 81
+        '-': (80, 81),      # Syntonic comma, factor of 5
+        '+': (81, 80),      # '-' = 5 * 2^4 / 3^4
 
-        '7': (7, 1, -6, 2),     # Septimal comma
-        'L': (7, -1, 6, -2),    # '7' = 63 / 64
+        '7': (63, 64),      # Septimal comma, factor of 7
+        'L': (64, 63),      # '7' = 7 * 3^2 / 2^6
 
-        'u': (11, 1, -5, 1),    # Undecimal comma
-        'd': (11, -1, 5, -1),   # 'u' = 33 / 32
+        'u': (33, 32),      # Undecimal comma, factor of 11
+        'd': (32, 33),      # 'u' = 11 * 3 / 2^5
 
-        '3': (13, 1, -10, 4),   # Tridecimal comma
-        'e': (13, -1, 10, -4),  # '3' = 1053 / 1024
+        '3': (1053, 1024),  # Tridecimal comma, factor of 13
+        'e': (1024, 1053),  # '3' = 13 * 3^4 / 2^10
     }
-
-    # Accounts for the note name itself contributing factors
-    factors[2], factors[3] = note_name_to_factor(note_raw[0])
 
     # Applies each accidental iteratively
     for c in note_raw[1:]:
-        factor, amount, correction2, correction3 = accidentals.get(c)
-        factors[factor] += amount
-        factors[2] += correction2
-        factors[3] += correction3
+        note_fraction = multiply(note_fraction, accidentals.get(c))
 
-    return factors
-
-
-def evaluate_factors(factors: dict[int, int]) -> float:
-    total_factor = 1
-    for factor, amount in factors.items():
-        total_factor *= factor ** amount
-    return total_factor
+    return note_fraction
 
 
 def main() -> None:
@@ -98,11 +82,10 @@ def main() -> None:
 
     while note != "X":
         steps = note_to_12TET_steps(note)
-        print(f"12TET Steps: {steps}")
 
-        total_factor = evaluate_factors(parse_factors(note))
-        print(f"Ratio: {total_factor}")
-        total_cents = 12 * log2(total_factor)
+        total_factor = parse_input(note)
+        print(f"Ratio to C: {total_factor[0]} / {total_factor[1]}")
+        total_cents = 12 * log2(total_factor[0] / total_factor[1])
 
         diff = 100 * (total_cents - steps)
 
