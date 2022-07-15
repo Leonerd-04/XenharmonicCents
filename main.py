d73bab9fadd87c7cd8b8de2013a3284079d4f738
Ieonard04 <leonerd04.2718@gmail.com>
2022-07-14 17:51:00 -0700

    Added comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from math import log2
 
 
+# One step = one half-step (eg. C to Db)
+# Is allowed to be negative and greater than 12
 def note_to_12TET_steps(name: str) -> int:
     switch = {
         'C': 0,
@@ -23,6 +25,8 @@ def note_to_12TET_steps(name: str) -> int:
     return steps
 
 
+# Determines how many factors of 3 a natural note is away from C
+# Tuple is (factors of 2, factors of 3)
 def note_name_to_factor(name: str) -> tuple[int, int]:
     switch = {
         'F': (2, -1),
@@ -47,22 +51,32 @@ def parse_factors(note_raw: str) -> dict[int, int]:
         13: 0
     }
 
+    # Tuple structure is as unpacked below: (factor, amount, correction2, correction3)
+    # factor should always be a prime number and is the accidental's primary factor
+    # amount is the exponent of the primary factor, will generally be 1 or -1
+    # The two corrections are the factors of 2 and 3 respectively.
+    # Adjacent accidentals are inverses.
     accidentals = {
-        '#': (3, 7, -11, 0),
-        'b': (3, -7, 11, 0),
-        '-': (5, 1, 4, -4),
-        '+': (5, -1, -4, 4),
-        '7': (7, 1, -6, 2),
-        'L': (7, -1, 6, -2),
-        'u': (11, 1, -5, 1),
-        'd': (11, -1, 5, -1),
-        '3': (13, 1, -10, 4),
-        'e': (13, -1, 10, -4),
+        '#': (3, 7, -11, 0),    # Sharp and flat are pythagorean
+        'b': (3, -7, 11, 0),    # '#' = 2187 / 2048
 
+        '-': (5, 1, 4, -4),     # Syntonic comma
+        '+': (5, -1, -4, 4),    # '-' = 80 / 81
+
+        '7': (7, 1, -6, 2),     # Septimal comma
+        'L': (7, -1, 6, -2),    # '7' = 63 / 64
+
+        'u': (11, 1, -5, 1),    # Undecimal comma
+        'd': (11, -1, 5, -1),   # 'u' = 33 / 32
+
+        '3': (13, 1, -10, 4),   # Tridecimal comma
+        'e': (13, -1, 10, -4),  # '3' = 1053 / 1024
     }
 
+    # Accounts for the note name itself contributing factors
     factors[2], factors[3] = note_name_to_factor(note_raw[0])
 
+    # Applies each accidental iteratively
     for c in note_raw[1:]:
         factor, amount, correction2, correction3 = accidentals.get(c)
         factors[factor] += amount
